f3069236afa68da6068196b784c5fb647ffc6a41
Matheus Martins Sarmento <matheusms@pop-os.localdomain>
2021-02-03 22:51:22 -0300

    fix firebase

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { useState } from "react";
 import "./assets/scss/style.scss";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
+import { db } from "./config/firebase";
+import firebase from "firebase/app";
 
 function App() {
   const [todoInput, setTodoInput] = useState("");
 
-  function addTodo() {}
+  function addTodo(e) {
+    e.preventDefault();
+
+    db.collection("todos")
+      .add({
+        todo: todoInput,
+        inprogress: false,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+
+    setTodoInput("");
+  }
 
   return (
     <div className="App">
@@ -16,9 +35,12 @@ function App() {
         <TextField
           id="input-todo"
           label="Write a Todo"
+          value={todoInput}
           onChange={(e) => setTodoInput(e.target.value)}
         />
-        <Button type="submit" onClick={addTodo} />
+        <Button variant="contained" type="submit" onClick={addTodo}>
+          OI
+        </Button>
       </form>
     </div>
   );
